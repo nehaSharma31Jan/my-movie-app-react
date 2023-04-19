93e9f8b2bdbe9bd472a0a604ec808b9f83d1a1a7
NehaSharma <116463443+NehaSharma884@users.noreply.github.com>
2023-04-18 22:32:33 -0400

    errererreer

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// ReactDOM.createRoot(
-//   <AuthProvider>
-//     <App />
-//   </AuthProvider>,
-//   document.getElementById('root')
-
-
-  const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
+ReactDOM.render(
   <AuthProvider>
-   <App />
+    <App />
   </AuthProvider>,
+  document.getElementById('root')
 );
-
-document.getElementById('root')
-  
-
-
